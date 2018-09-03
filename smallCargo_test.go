a43258fdcc7fce189886cfa110fcb6bf3b59ab09
Alain Gilbert <alain.gilbert.15@gmail.com>
2018-09-03 00:25:31 -0700

    Add test for SC speed

--- smallCargo_test.go
+++ smallCargo_test.go
@@ -11,3 +11,8 @@ func TestSmallCargoConstructionTime(t *testing.T) {
 	assert.Equal(t, 164, sc.ConstructionTime(1, 7, Facilities{Shipyard: 4}))
 	assert.Equal(t, 328, sc.ConstructionTime(2, 7, Facilities{Shipyard: 4}))
 }
+
+func TestSmallCargoSpeed(t *testing.T) {
+	sc := newSmallCargo()
+	assert.Equal(t, 22000, sc.GetSpeed(Researches{CombustionDrive: 10, ImpulseDrive: 6}))
+}
