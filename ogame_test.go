b60d05dc7cf586140f18dacea8ef4ce77b8f85b2
Alain Gilbert <alain.gilbert.15@gmail.com>
2018-09-22 00:06:35 -0700

    move code

--- ogame_test.go
+++ ogame_test.go
@@ -8,6 +8,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestExtractDefense(t *testing.T) {
+	pageHTMLBytes, _ := ioutil.ReadFile("samples/defence.html")
+	defense, _ := extractDefense(string(pageHTMLBytes))
+	assert.Equal(t, 1, defense.RocketLauncher)
+	assert.Equal(t, 2, defense.LightLaser)
+	assert.Equal(t, 3, defense.HeavyLaser)
+	assert.Equal(t, 4, defense.GaussCannon)
+	assert.Equal(t, 5, defense.IonCannon)
+	assert.Equal(t, 6, defense.PlasmaTurret)
+	assert.Equal(t, 0, defense.SmallShieldDome)
+	assert.Equal(t, 0, defense.LargeShieldDome)
+	assert.Equal(t, 7, defense.AntiBallisticMissiles)
+	assert.Equal(t, 8, defense.InterplanetaryMissiles)
+}
+
 func TestProductionRatio(t *testing.T) {
 	ratio := productionRatio(
 		Temperature{-23, 17},
--- planet_test.go
+++ planet_test.go
@@ -1,7 +1,6 @@
 package ogame
 
 import (
-	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,19 +14,4 @@ func TestFields_HasFieldAvailable(t *testing.T) {
 func TestTemperature_Mean(t *testing.T) {
 	assert.Equal(t, 5, Temperature{Min: 0, Max: 10}.Mean())
 	assert.Equal(t, 0, Temperature{Min: -10, Max: 10}.Mean())
-}
-
-func TestExtractDefense(t *testing.T) {
-	pageHTMLBytes, _ := ioutil.ReadFile("samples/defence.html")
-	defense, _ := extractDefense(string(pageHTMLBytes))
-	assert.Equal(t, 1, defense.RocketLauncher)
-	assert.Equal(t, 2, defense.LightLaser)
-	assert.Equal(t, 3, defense.HeavyLaser)
-	assert.Equal(t, 4, defense.GaussCannon)
-	assert.Equal(t, 5, defense.IonCannon)
-	assert.Equal(t, 6, defense.PlasmaTurret)
-	assert.Equal(t, 0, defense.SmallShieldDome)
-	assert.Equal(t, 0, defense.LargeShieldDome)
-	assert.Equal(t, 7, defense.AntiBallisticMissiles)
-	assert.Equal(t, 8, defense.InterplanetaryMissiles)
 }
