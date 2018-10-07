be9a510c893ca7e63c1ff220a357ffcd4ee493bb
Alain Gilbert <alain.gilbert.15@gmail.com>
2018-10-07 05:46:50 -0700

    moon fields

--- moon.go
+++ moon.go
@@ -1,5 +1,13 @@
 package ogame
 
+type MoonID int
+
 // Moon ogame moon object
 type Moon struct {
+	ID         MoonID
+	Img        string
+	Name       string
+	Diameter   int
+	Coordinate Coordinate
+	Fields     Fields
 }
