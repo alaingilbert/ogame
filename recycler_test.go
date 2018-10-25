08ebe09b3edceb3f9905cc4001379e0ce0e8fc90
apg258 <apg258@my.open.ac.uk>
2018-10-25 18:50:29 +0100

    fix formula & add test

---
+++ recycler_test.go
@@ -0,0 +1,14 @@
+package ogame
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRecyclerSpeed(t *testing.T) {
+	r := newRecycler()
+	assert.Equal(t, 3000, r.GetSpeed(Researches{CombustionDrive: 5, ImpulseDrive: 1, HyperspaceDrive: 1}))
+	assert.Equal(t, 17600, r.GetSpeed(Researches{CombustionDrive: 1, ImpulseDrive: 17, HyperspaceDrive: 10}))
+	assert.Equal(t, 33000, r.GetSpeed(Researches{CombustionDrive: 1, ImpulseDrive: 17, HyperspaceDrive: 15}))
+}
