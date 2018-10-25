4f00a76f8c7f83359e61e8402c71e796f5f0ddf4
apg258 <apg258@my.open.ac.uk>
2018-10-25 20:34:17 +0100

    small cargo fix, recycler test fix

--- recycler_test.go
+++ recycler_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestRecyclerSpeed(t *testing.T) {
 	r := newRecycler()
-	assert.Equal(t, 3000, r.GetSpeed(Researches{CombustionDrive: 5, ImpulseDrive: 1, HyperspaceDrive: 1}))
+	assert.Equal(t, 3200, r.GetSpeed(Researches{CombustionDrive: 6, ImpulseDrive: 1, HyperspaceDrive: 1}))
 	assert.Equal(t, 17600, r.GetSpeed(Researches{CombustionDrive: 1, ImpulseDrive: 17, HyperspaceDrive: 10}))
 	assert.Equal(t, 33000, r.GetSpeed(Researches{CombustionDrive: 1, ImpulseDrive: 17, HyperspaceDrive: 15}))
 }
