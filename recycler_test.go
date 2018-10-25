c3d0b1722bed924228e66cbda823152ed82361ef
apg258 <apg258@my.open.ac.uk>
2018-10-25 21:14:53 +0100

    more tests and bomber speed fix

--- recycler_test.go
+++ recycler_test.go
@@ -11,4 +11,6 @@ func TestRecyclerSpeed(t *testing.T) {
 	assert.Equal(t, 3200, r.GetSpeed(Researches{CombustionDrive: 6, ImpulseDrive: 1, HyperspaceDrive: 1}))
 	assert.Equal(t, 17600, r.GetSpeed(Researches{CombustionDrive: 1, ImpulseDrive: 17, HyperspaceDrive: 10}))
 	assert.Equal(t, 33000, r.GetSpeed(Researches{CombustionDrive: 1, ImpulseDrive: 17, HyperspaceDrive: 15}))
+	assert.Equal(t, 18400, r.GetSpeed(Researches{CombustionDrive: 1, ImpulseDrive: 18, HyperspaceDrive: 10}))
+	assert.Equal(t, 34800, r.GetSpeed(Researches{CombustionDrive: 1, ImpulseDrive: 17, HyperspaceDrive: 16}))
 }
