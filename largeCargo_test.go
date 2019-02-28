867297e1f6299b619e799a8f2c1d624e97fe4fd8
apg258 <alexpg258@gmail.com>
2019-02-28 18:34:13 +0000

    tests

--- largeCargo_test.go
+++ largeCargo_test.go
@@ -9,6 +9,10 @@ import (
 func TestLargeCargo_GetSpeed(t *testing.T) {
 	lc := newLargeCargo()
 	assert.Equal(t, 12000, lc.GetSpeed(Researches{}))
+}
+
+func TestLargeCargo_GetCargoCapacity(t *testing.T) {
+	lc := newLargeCargo()
 	assert.Equal(t, 29000, lc.GetCargoCapacity(Researches{HyperspaceTechnology: 8}))
 	assert.Equal(t, 30000, lc.GetCargoCapacity(Researches{HyperspaceTechnology: 10}))
 }
