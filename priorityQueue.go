124924ec0e915ea98f52e8bf0e6b2efa486896c6
Alain Gilbert <alain.gilbert.15@gmail.com>
2019-03-09 17:21:34 -0800

    priority queue in own file

---
+++ priorityQueue.go
@@ -0,0 +1,43 @@
+package ogame
+
+// item ...
+type item struct {
+	canBeProcessedCh chan struct{}
+	isDoneCh         chan struct{}
+	priority         int
+	index            int // The index of the item in the heap.
+}
+
+// A priorityQueue implements heap.Interface and holds Items.
+type priorityQueue []*item
+
+func (pq priorityQueue) Len() int { return len(pq) }
+
+func (pq priorityQueue) Less(i, j int) bool {
+	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
+	return pq[i].priority > pq[j].priority
+}
+
+func (pq priorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+// Push ...
+func (pq *priorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+// Pop ...
+func (pq *priorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
