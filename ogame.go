1446d977e5d8edcce0f8330365f2dbfa5544d7ca
Alain Gilbert <alain.gilbert.15@gmail.com>
2019-08-31 01:00:37 -0700

    break out of infinite loop if account is not found

--- ogame.go
+++ ogame.go
@@ -1004,6 +1004,9 @@ func (b *OGame) withRetry(fn func() error) error {
 				retry(err)
 				if loginErr := b.wrapLogin(); loginErr != nil {
 					b.error(loginErr.Error()) // log error
+					if loginErr == ErrAccountNotFound {
+						return ErrAccountNotFound
+					}
 					continue
 				}
 				continue
