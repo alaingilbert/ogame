55b501fdebd19747595e4ee434583fe5628887e1
Alain Gilbert <alain.gilbert.15@gmail.com>
2019-09-02 21:20:55 -0700

    fix interface

--- interfaces.go
+++ interfaces.go
@@ -89,8 +89,8 @@ type Wrapper interface {
 	// Planet or Moon functions
 	GetResources(CelestialID) (Resources, error)
 	GetResourcesDetails(CelestialID) (ResourcesDetails, error)
-	SendFleet(celestialID CelestialID, ships []Quantifiable, speed Speed, where Coordinate, mission MissionID, resources Resources, expeditiontime int) (Fleet, error)
-	EnsureFleet(celestialID CelestialID, ships []Quantifiable, speed Speed, where Coordinate, mission MissionID, resources Resources, expeditiontime int) (Fleet, error)
+	SendFleet(celestialID CelestialID, ships []Quantifiable, speed Speed, where Coordinate, mission MissionID, resources Resources, expeditiontime, unionID int) (Fleet, error)
+	EnsureFleet(celestialID CelestialID, ships []Quantifiable, speed Speed, where Coordinate, mission MissionID, resources Resources, expeditiontime, unionID int) (Fleet, error)
 	Build(celestialID CelestialID, id ID, nbr int) error
 	BuildCancelable(CelestialID, ID) error
 	BuildProduction(celestialID CelestialID, id ID, nbr int) error
