0ba62e37fd01e2bb483c35fab369f5d4d2425dd1
Alain Gilbert <alain.gilbert.15@gmail.com>
2019-12-10 19:42:55 -0800

    Fix espionage report resources parsing for v7.1

--- extractor_v71.go
+++ extractor_v71.go
@@ -1,5 +1,12 @@
 package ogame
 
+import (
+	"bytes"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
 // ExtractorV71 ...
 type ExtractorV71 struct {
 	ExtractorV7
@@ -12,4 +19,15 @@ func NewExtractorV71() *ExtractorV71 {
 
 func (e ExtractorV71) ExtractResourcesDetails(pageHTML []byte) (out ResourcesDetails, err error) {
 	return extractResourcesDetailsV71(pageHTML)
+}
+
+// ExtractEspionageReport ...
+func (e ExtractorV71) ExtractEspionageReport(pageHTML []byte, location *time.Location) (EspionageReport, error) {
+	doc, _ := goquery.NewDocumentFromReader(bytes.NewReader(pageHTML))
+	return e.ExtractEspionageReportFromDoc(doc, location)
+}
+
+// ExtractEspionageReportFromDoc ...
+func (e ExtractorV71) ExtractEspionageReportFromDoc(doc *goquery.Document, location *time.Location) (EspionageReport, error) {
+	return extractEspionageReportFromDocV71(doc, location)
 }
