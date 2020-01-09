717ec6ae29ec9fc901a9d561c17be70678a4bd4a
Alain Gilbert <alain.gilbert.15@gmail.com>
2020-01-08 18:51:31 -0800

    simplify code

--- baseShip.go
+++ baseShip.go
@@ -23,26 +23,32 @@ func (b BaseShip) GetFuelConsumption() int64 {
 
 // GetSpeed returns speed of the ship
 func (b BaseShip) GetSpeed(techs Researches) int64 {
-	var techDriveLvl int64 = 0
+	techDriveLvl := 0.0
+	driveFactor := 0.2
+	baseSpeed := float64(b.BaseSpeed)
+	multiplier := int64(1)
 	if b.ID == SmallCargoID && techs.ImpulseDrive >= 5 {
-		baseSpeed := 10000
-		return int64(float64(baseSpeed) + (float64(baseSpeed)*0.2)*float64(techs.ImpulseDrive))
+		baseSpeed = 10000
+		techDriveLvl = float64(techs.ImpulseDrive)
 	} else if b.ID == BomberID && techs.HyperspaceDrive >= 8 {
-		baseSpeed := 5000
-		return int64(float64(baseSpeed) + (float64(baseSpeed)*0.3)*float64(techs.HyperspaceDrive))
+		baseSpeed = 5000
+		techDriveLvl = float64(techs.HyperspaceDrive)
+		driveFactor = 0.3
 	} else if b.ID == RecyclerID && techs.HyperspaceDrive >= 15 {
-		return int64(float64(b.BaseSpeed)+(float64(b.BaseSpeed)*0.3)*float64(techs.HyperspaceDrive)) * 3
+		techDriveLvl = float64(techs.HyperspaceDrive)
+		multiplier = 3
+		driveFactor = 0.3
 	} else if b.ID == RecyclerID && techs.ImpulseDrive >= 17 {
-		return int64(float64(b.BaseSpeed)+(float64(b.BaseSpeed)*0.2)*float64(techs.ImpulseDrive)) * 2
+		techDriveLvl = float64(techs.ImpulseDrive)
+		multiplier = 2
 	} else if minLvl, ok := b.Requirements[CombustionDrive.ID]; ok {
-		techDriveLvl = MaxInt(techs.CombustionDrive, minLvl)
-		return int64(float64(b.BaseSpeed) + (float64(b.BaseSpeed)*0.1)*float64(techDriveLvl))
+		techDriveLvl = float64(MaxInt(techs.CombustionDrive, minLvl))
+		driveFactor = 0.1
 	} else if minLvl, ok := b.Requirements[ImpulseDrive.ID]; ok {
-		techDriveLvl = MaxInt(techs.ImpulseDrive, minLvl)
-		return int64(float64(b.BaseSpeed) + (float64(b.BaseSpeed)*0.2)*float64(techDriveLvl))
+		techDriveLvl = float64(MaxInt(techs.ImpulseDrive, minLvl))
 	} else if minLvl, ok := b.Requirements[HyperspaceDrive.ID]; ok {
-		techDriveLvl = MaxInt(techs.HyperspaceDrive, minLvl)
-		return int64(float64(b.BaseSpeed) + (float64(b.BaseSpeed)*0.3)*float64(techDriveLvl))
+		techDriveLvl = float64(MaxInt(techs.HyperspaceDrive, minLvl))
+		driveFactor = 0.3
 	}
-	return int64(float64(b.BaseSpeed) + (float64(b.BaseSpeed)*0.2)*float64(techDriveLvl))
+	return int64(baseSpeed+(baseSpeed*driveFactor)*techDriveLvl) * multiplier
 }
