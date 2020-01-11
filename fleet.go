800a3b38df8e9ffbf2ad8938497e780b70a52ff4
Kane67 <Kane67@troniac.de>
2020-01-12 00:46:08 +0100

    Revert "Update fleet.go"
    This reverts commit 106d075987884845105b174e387fba7763ffbae6.

--- fleet.go
+++ fleet.go
@@ -14,33 +14,3 @@ type Fleet struct {
 	UnionID        int64
 	TargetPlanetID int64
 }
-
-// Fleet is not returning and we can calculate the in air for sec. The function returns -1 if the fleet is returning!
-func (f Fleet) InAir() int64 {
-	if !f.ReturnFlight {
-		TotalFleetFlightTime := (f.BackIn - f.ArriveIn)
-		ArriveAt := time.Now().Unix() + f.ArriveIn
-		FleetStartAt := (ArriveAt - TotalFleetFlightTime)
-		InAirForSec := time.Now().Unix() - FleetStartAt
-		return InAirForSec
-	}
-	return 0
-}
-
-// Fleet is not returning and we can calculate the total time the Fleet needs to arrive. The function returns -1 if the fleet is returning!
-func (f Fleet) Flighttime() int64 {
-	if !f.ReturnFlight {
-		return (f.BackIn - f.ArriveIn)
-	} else {
-		return 0
-	}
-}
-
-// Fleet calculate the ArriveAt Unix.Timestamp for returning and not returning fleets.
-func (f Fleet) ArriveAt() int64 {
-	if f.ReturnFlight {
-		return time.Now().Unix() + f.BackIn
-	} else {
-		return time.Now().Unix() + (f.BackIn - f.ArriveIn)
-	}
-}
