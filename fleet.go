44e53b684e5fdf19fa75905d3b35ea1a4c4c5bc5
Kane67 <Kane67@troniac.de>
2020-01-12 01:12:07 +0100

    Update fleet.go
    **Added Functions**
    
    - InAir: returns the time for the fleet since start. (only not returning fleets)
    - Flighttime: The total Flighttime for a fleet. (only not returning fleets)
    - ArriveAt: The Unix Timestamp of the arrival time for a fleet. For returning and not returning fleets.

--- fleet.go
+++ fleet.go
@@ -12,3 +12,33 @@ type Fleet struct {
 	ArriveIn     int
 	BackIn       int
 }
+
+// Fleet is not returning and we can calculate the in air time in sec. The function returns 0 if the fleet is returning!
+func (f Fleet) InAir() int64 {
+	if !f.ReturnFlight {
+		TotalFleetFlightTime := (f.BackIn - f.ArriveIn)
+		ArriveAt := time.Now().Unix() + f.ArriveIn
+		FleetStartAt := (ArriveAt - TotalFleetFlightTime)
+		InAirForSec := time.Now().Unix() - FleetStartAt
+		return InAirForSec
+	}
+	return 0
+}
+
+// Fleet is not returning and we can calculate the total time the Fleet needs to arrive. The function returns 0 if the fleet is returning!
+func (f Fleet) Flighttime() int64 {
+	if !f.ReturnFlight {
+		return (f.BackIn - f.ArriveIn)
+	} else {
+		return 0
+	}
+}
+
+// Fleet calculate the ArriveAt Unix.Timestamp for returning and not returning fleets.
+func (f Fleet) ArriveAt() int64 {
+	if f.ReturnFlight {
+		return time.Now().Unix() + f.BackIn
+	} else {
+		return time.Now().Unix() + (f.BackIn - f.ArriveIn)
+	}
+}
