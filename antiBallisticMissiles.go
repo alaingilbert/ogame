50057c2f908e290634a98ec88535017ff9221a2f
Alain Gilbert <alain.gilbert.15@gmail.com>
2020-02-24 00:08:53 -0800

    Fix anti ballistic missiles price

--- antiBallisticMissiles.go
+++ antiBallisticMissiles.go
@@ -8,7 +8,7 @@ func newAntiBallisticMissiles() *antiBallisticMissiles {
 	d := new(antiBallisticMissiles)
 	d.Name = "anti ballistic missiles"
 	d.ID = AntiBallisticMissilesID
-	d.Price = Resources{Metal: 8000, Crystal: 2000}
+	d.Price = Resources{Metal: 8000, Deuterium: 2000}
 	d.StructuralIntegrity = 8000
 	d.ShieldPower = 1
 	d.WeaponPower = 1
