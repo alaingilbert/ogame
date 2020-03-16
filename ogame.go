6db6ea9ce524f99e807018c39da625c1149d5e80
flashover <webfreakz+github@gmail.com>
2020-03-16 06:41:17 +0100

    Changed UserAgent. Changed GetStaticHandler() to use bot.Client.Do() … (#68)
    * Changed UserAgent. Changed GetStaticHandler() to use bot.Client.Do() with the correct User-Agent instead of using raw HTTP.Get() which sends wrong User-Agent.
    
    * Swapped HEAD -> GET -request
    
    Co-authored-by: Flashover <flashover@localhost>

--- ogame.go
+++ ogame.go
@@ -127,9 +127,9 @@ type Preferences struct {
 }
 
 const defaultUserAgent = "" +
-	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) " +
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64)  " +
 	"AppleWebKit/537.36 (KHTML, like Gecko) " +
-	"Chrome/79.0.3945.130 " +
+	"Chrome/80.0.3987.132 " +
 	"Safari/537.36"
 
 type options struct {
