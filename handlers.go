a0251c4bac0fd16d95b502313cb5f985e9010dea
0xE232FE <CrynogarTM@gmail.com>
2020-08-23 21:22:37 +0200

    Fix malfunction of GetStaticHandler (#80)
    * Fix Marketplace hostname in replaceHostname Function
    
    The reason of this is double quoted/escaped url in javascript file. That prevents several OGame Page Browser functions like filter, Sort and page change of market lists.
    
    * Update handlers.go
    
    typo doubleescapedServerURL doubleEscapedServerURL
    
    * Update handlers.go
    
    Typo doubleescapedAPINewHostname -> doubleEscapedAPINewHostname
    
    Co-authored-by: default <default>

--- handlers.go
+++ handlers.go
@@ -891,9 +891,12 @@ func replaceHostname(bot *OGame, html []byte) []byte {
 	serverURLBytes := []byte(bot.serverURL)
 	apiNewHostnameBytes := []byte(bot.apiNewHostname)
 	escapedServerURL := bytes.Replace(serverURLBytes, []byte("/"), []byte(`\/`), -1)
+	doubleEscapedServerURL := bytes.Replace(serverURLBytes, []byte("/"), []byte("\\\\\\/"), -1)
 	escapedAPINewHostname := bytes.Replace(apiNewHostnameBytes, []byte("/"), []byte(`\/`), -1)
+	doubleEscapedAPINewHostname := bytes.Replace(apiNewHostnameBytes, []byte("/"), []byte("\\\\\\/"), -1)
 	html = bytes.Replace(html, serverURLBytes, apiNewHostnameBytes, -1)
 	html = bytes.Replace(html, escapedServerURL, escapedAPINewHostname, -1)
+	html = bytes.Replace(html, doubleEscapedServerURL, doubleEscapedAPINewHostname, -1)
 	return html
 }
 
