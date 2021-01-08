6a725bbf7d79873bf9353e1c8d42f8b3a2810587
Alain Gilbert <alain.gilbert.15@gmail.com>
2021-01-08 01:10:22 -0800

    Fix timezone discovery

--- extracts_v6.go
+++ extracts_v6.go
@@ -1258,7 +1258,7 @@ func extractServerTimeFromDocV6(doc *goquery.Document) (time.Time, error) {
 
 	u1 := time.Now().UTC().Unix()
 	u2 := serverTime.Unix()
-	n := int(math.Round(float64(u2-u1)/15)) * 15
+	n := int(math.Round(float64(u2-u1)/900)) * 900 // u2-u1 should be close to 0, round to nearest 15min difference
 
 	serverTime = serverTime.Add(time.Duration(-n) * time.Second).In(time.FixedZone("OGT", n))
 
