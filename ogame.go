5811518baff6141721abe6c7ae03b4f194031d57
Alain Gilbert <alain.gilbert.15@gmail.com>
2021-01-27 05:32:15 -0800

    logout of ogame only

--- ogame.go
+++ ogame.go
@@ -1656,7 +1656,7 @@ func (m ChatMsg) String() string {
 
 func (b *OGame) logout() {
 	_, _ = b.getPage(LogoutPage, CelestialID(0))
-	b.Client.Jar.(*cookiejar.Jar).RemoveAll()
+	b.Client.Jar.(*cookiejar.Jar).Save()
 	if atomic.CompareAndSwapInt32(&b.isLoggedInAtom, 1, 0) {
 		select {
 		case <-b.closeChatCh:
