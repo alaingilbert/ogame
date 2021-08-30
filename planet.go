fdbfbce0e2a740742c0803b0ad53bae4a419e091
0xE232FE <CrynogarTM@gmail.com>
2021-08-30 16:34:37 +0200

    fixing Interfaces anf FleetBuilder and Planet calls for FlightTime function with holdingTime parameter

--- planet.go
+++ planet.go
@@ -194,8 +194,8 @@ func (p *Planet) GetResourcesProductions() (Resources, error) {
 }
 
 // FlightTime calculate flight time and fuel needed
-func (p *Planet) FlightTime(destination Coordinate, speed Speed, ships ShipsInfos, missionID MissionID) (secs, fuel int64) {
-	return p.ogame.FlightTime(p.Coordinate, destination, speed, ships, missionID)
+func (p *Planet) FlightTime(destination Coordinate, speed Speed, ships ShipsInfos, missionID MissionID, holdingTime int64) (secs, fuel int64) {
+	return p.ogame.FlightTime(p.Coordinate, destination, speed, ships, missionID, holdingTime)
 }
 
 // SendIPM send interplanetary missiles
