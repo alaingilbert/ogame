2615a9bf11bc32f4ea63b464e7c34ac667a35f77
David <143167059+Davx1999@users.noreply.github.com>
2023-09-04 22:16:59 +0200

    * new galaxy page
    - add new galaxy ajax page (GalaxyAjaxPageName)

--- pkg/wrapper/fetcher.go
+++ pkg/wrapper/fetcher.go
@@ -42,6 +42,7 @@ const (
 	FetchEventboxAjaxPageName      = "fetchEventbox"
 	FetchResourcesAjaxPageName     = "fetchResources"
 	GalaxyContentAjaxPageName      = "galaxyContent"
+	GalaxyAjaxPageName             = "galaxy"
 	EventListAjaxPageName          = "eventList"
 	AjaxChatAjaxPageName           = "ajaxChat"
 	NoticesAjaxPageName            = "notices"
