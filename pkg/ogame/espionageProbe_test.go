7e11f41f668c8bdc6d5c74294b1048b6228477de
Alain Gilbert <alain.gilbert.15@gmail.com>
2024-01-26 13:36:02 -0800

    add test for probe cargo

--- pkg/ogame/espionageProbe_test.go
+++ pkg/ogame/espionageProbe_test.go
@@ -12,3 +12,8 @@ func TestEspionageProbeFuelConsumption(t *testing.T) {
 	assert.Equal(t, int64(0), ep.GetFuelConsumption(Researches{}, 1, true))
 	assert.Equal(t, int64(0), ep.GetFuelConsumption(Researches{}, 0.5, false))
 }
+
+func TestEspionageProbe_GetCargoCapacity(t *testing.T) {
+	ep := newEspionageProbe()
+	assert.Equal(t, int64(8), ep.GetCargoCapacity(Researches{HyperspaceTechnology: 14}, true, false, false))
+}
