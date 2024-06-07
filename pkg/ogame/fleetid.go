9012093bee0151e83cca88994dc6dc1dd0a67f2c
Alain Gilbert <alain.gilbert.15@gmail.com>
2024-06-06 23:39:21 -0700

    IsSet helper for FleetID

--- pkg/ogame/fleetid.go
+++ pkg/ogame/fleetid.go
@@ -7,6 +7,12 @@ import (
 // FleetID represent a fleet id
 type FleetID int64
 
+// String ...
 func (f FleetID) String() string {
 	return strconv.FormatInt(int64(f), 10)
+}
+
+// IsSet ...
+func (f FleetID) IsSet() bool {
+	return f > 0
 }
