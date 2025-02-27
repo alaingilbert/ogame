90d4a7b490698e78d5cbd53131284f83d089bb6a
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-02-27 12:02:07 -0800

    reusable i18n strings

--- pkg/ogame/utils.go
+++ pkg/ogame/utils.go
@@ -63,20 +63,24 @@ func unique(s string) string {
 	return strings.Join(arr, "")
 }
 
+// MetalStrings ...
+var MetalStrings = []string{"metalli", "métal", "metal", "metall", "kov", "kovy", "металл", "metallo", "metaal", "メタル", "金屬", "μέταλλο"}
+
+// CrystalStrings ...
+var CrystalStrings = []string{"kristalli", "kristal", "cristal", "crystal", "krystal", "kryštály", "kryształ", "kristall", "krystall", "cristallo", "кристалл", "krystaly", "クリスタル", "晶體", "κρύσταλλο"}
+
+// DeuteriumStrings ...
+var DeuteriumStrings = []string{"deuter", "deuterij", "deutérium", "deuterium", "deuterio", "дейтерий", "deutério", "deuteriu", "デューテリウム", "重氫", "δευτέριο"}
+
 // IsStrMetal ...
 func IsStrMetal(name string) bool {
-	arr := []string{"metalli", "métal", "metal", "metall", "kov", "kovy", "металл", "metallo", "metaal", "メタル", "金屬", "μέταλλο"}
-	return utils.InArr(name, arr)
+	return utils.InArr(name, MetalStrings)
 }
 
 // IsStrCrystal ...
 func IsStrCrystal(name string) bool {
-	arr := []string{"kristalli", "kristal", "cristal", "crystal", "krystal", "kryštály", "kryształ", "kristall", "krystall", "cristallo", "кристалл", "krystaly", "クリスタル", "晶體", "κρύσταλλο"}
-	return utils.InArr(name, arr)
+	return utils.InArr(name, CrystalStrings)
 }
-
-// DeuteriumStrings ...
-var DeuteriumStrings = []string{"deuter", "deuterij", "deutérium", "deuterium", "deuterio", "дейтерий", "deutério", "deuteriu", "デューテリウム", "重氫", "δευτέριο"}
 
 // IsStrDeuterium ...
 func IsStrDeuterium(name string) bool {
